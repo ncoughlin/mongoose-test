46bf94dde60339ac026944f0735a20b559fd5134
coughlin.nicholas@gmail.com <you@example.com>
2019-11-19 18:12:59 +0000

    successfully adding items to mongoDB

--- mongoose.js
+++ mongoose.js
@@ -26,7 +26,7 @@ var iceCreamSchema = new mongoose.Schema({
   });
   
 /*------------------------------------------
-METHODS - Must Go Before COMPILE
+CUSTOM METHODS - Must Go Before COMPILE
 ------------------------------------------*/
 
 iceCreamSchema.methods.display = function () {
@@ -43,7 +43,7 @@ var IceCream = mongoose.model('IceCream', iceCreamSchema);
 
 
 /*------------------------------------------
-DATA
+SAMPLE DATA
 ------------------------------------------*/
 
 // add an ice cream to the DB
@@ -54,14 +54,37 @@ var rockyRoad = new IceCream({
     description: "Rocky road ice cream is a chocolate flavored ice cream. Though there are variations from the original flavor, it is traditionally composed of chocolate ice cream, nuts, and whole or diced marshmallows."
 });
 
-console.log(rockyRoad.flavor);
-// our method as defined above
+var cookieDough = new IceCream({
+    flavor:  "Cookie Dough",
+    delicious: true,
+    calories: 750,
+    description: "Chocolate chip cookie dough ice cream is a popular ice cream flavor in which unbaked chunks of chocolate chip cookie dough are embedded in vanilla flavored ice cream."
+});
+
+// our custom method as defined above
 rockyRoad.display()
+cookieDough.display()
+
+/*------------------------------------------
+SAVE DATA TO MONGODB - .save Method
+------------------------------------------*/
+
+rockyRoad.save(function (err, rockyRoad){
+  if (err) return console.error(err);
+  rockyRoad.display();
+});
+
+cookieDough.save(function (err, rockyRoad){
+  if (err) return console.error(err);
+  cookieDough.display();
+});
 
 
+/*------------------------------------------
+DISPLAY DATA - .find Method
+------------------------------------------*/
 
-
-
-
-
-
+IceCream.find(function (err, iceCreams){
+  if (err) return console.error(err);
+  console.log(iceCreams);
+});
